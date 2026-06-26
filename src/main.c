5990265022f7083ad668029b76dc7bcc139841b6
Rosie <jayjaygames40@gmail.com>
2026-06-26 13:34:35 -0700

    Refactored a whole bunch of the fucken window shit so that it is now held by a context in main!

--- src/main.c
+++ src/main.c
@@ -4,22 +4,29 @@
 #define WIN_WIDTH  (TILES_X * TILE_SIZE)
 #define WIN_HEIGHT (TILES_Y * TILE_SIZE)
 
+static struct window window = {
+        .name   = "",
+        .handle = NULL,
+        .width  = 0u,
+        .height = 0u,
+};
+
 int main(void)
 {
-        window_init(WIN_WIDTH, WIN_HEIGHT);
+        window_init(&window, "Minesweeper", WIN_WIDTH, WIN_HEIGHT);
         tiles_init();
 
-        while (window_running_get()) {
+        while (window_running_get(&window)) {
                 window_clear(0.2f, 0.3f, 0.2f, 1.0f);
 
-                tiles_update(WIN_WIDTH, WIN_HEIGHT);
-                tiles_draw(WIN_WIDTH, WIN_HEIGHT);
+                tiles_update(&window);
+                tiles_draw(&window);
 
-                window_display();
+                window_display(&window);
         }
 
         tiles_terminate();
-        window_terminate();
+        window_terminate(&window);
 
         return 0;
 }
